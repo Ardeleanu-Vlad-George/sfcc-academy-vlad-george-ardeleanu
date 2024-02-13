442430e171f89e50926f74d5e18fd755842a2a41
Ardeleanu-Vlad-George <ardeleanu_vlad@ymail.com>
2024-02-13 15:21:31 +0200

    Managing to find product with 'ProductMgr'

--- cartridges/app_training/cartridge/controllers/Training.js
+++ cartridges/app_training/cartridge/controllers/Training.js
@@ -13,8 +13,23 @@ server.get('HelloWorld', function (req, res, next) {
 
 server.get('SessionVariable', function(req, res, next){
     res.render('training/session_var')
-    
     return next();
 });
+
+
+server.get('ShowProduct', function(req, res, next){
+    const ProductMgr = require('dw/catalog/ProductMgr');
+    
+    const selected = ProductMgr.getProduct(req.querystring.pid);
+
+    res.render('training/finding_product', {
+        givenID : req.querystring.pid,
+        found : !selected ? "NO" : "YES",
+        product : selected
+    });
+
+    return next();
+});
+
 
 module.exports = server.exports();
